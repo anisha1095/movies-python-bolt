9ee9fd99264c98709a4c6fe0199ff00ff1cded83
Anisha Jauhari <anishajauhari10@gmail.com>
2021-03-11 11:10:19 -0600

    initial2

--- movies.py
+++ movies.py
@@ -63,7 +63,7 @@ def serialize_cast(cast):
 def get_graph():
     db = get_db()
     results = db.read_transaction(lambda tx: list(tx.run("MATCH (m:Movie)<-[:ACTED_IN]-(a:Person) "
-                                                         "RETURN m.title as movie, collect(a.name) as cast "
+                                                         "RETURN m.title as movie, collect(a.name) as cast, count(a) as weight "
                                                          "LIMIT $limit", {
                                                              "limit": request.args.get("limit",
                                                                                        100)})))
@@ -71,18 +71,19 @@ def get_graph():
     rels = []
     i = 0
     for record in results:
-        nodes.append({"title": record["movie"], "label": "movie"})
+        nodes.append({"title": record["movie"], "label": "movie", "weight": record["weight"]})
         target = i
         i += 1
         for name in record['cast']:
-            actor = {"title": name, "label": "actor"}
+            actor = {"title": name, "label": "actor", "weight": record["weight"]}
             try:
                 source = nodes.index(actor)
             except ValueError:
                 nodes.append(actor)
                 source = i
                 i += 1
-            rels.append({"source": source, "target": target})
+
+            rels.append({"source": source, "target": target, "weight": 2})
     return Response(dumps({"nodes": nodes, "links": rels}),
                     mimetype="application/json")
 
